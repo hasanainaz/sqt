db348b82d9d1d47b2c7e09743155e8bfd38b564d
Hasanainaz <60304214+hasanainaz@users.noreply.github.com>
2022-08-04 08:17:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from distutils.core import setup
 
+setup(
+    name = 'SQT',
+    packages = ['sqtpy'],
+    version = '0.0.1', 
+    description = 'sqtpy int16',
+    author = 'Ahmad Hasanain',
+    author_email = 'hasanainaz@gmail.com',
+    url = 'https://github.com/hasanainaz/sqt',
+    download_url = 'https://github.com/hasanainaz/sqt/archive/refs/tags/0.0.1.tar.gz',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
